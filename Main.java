c72d112999ac04f7d7ced295902790d2a925584c
Velisarios1234 <fafoutis@csd.auth.gr>
2022-02-07 22:07:25 +0200

    project updated

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import javax.swing.JFrame;
+
+import GUI.*;
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+      
+        new LoginPage();
     }
 }
